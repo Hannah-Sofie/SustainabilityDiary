56025bf64bf9488cbf03ea55823debff9f37236c
hannah-sofie <hannah-sofie@live.no>
2024-03-31 21:49:48 +0200

    Working on reflections

--- client/src/components/Reflection/Reflection.jsx
+++ client/src/components/Reflection/Reflection.jsx
@@ -1,42 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Reflection.css";
 
 function Reflections() {
   return (
     <div>
-      <div className="main-reflection">
-        <section className="container-reflection">
-          <h1>My Reflections</h1>
-          <div className="reflection-box">
-            <ul>
-              <li className="active">In progress</li>
-              <li>Explore</li>
-              <li>Incoming</li>
-              <li>Finished</li>
-            </ul>
-            <div className="reflection">
-              <div className="box">
-                <h3>HTML</h3>
-                <p>80% - progress</p>
-                <button>Continue</button>
-                <i className="fab fa-html5 html"></i>
+      <section className="main-reflection">
+        <div className="container-reflection">
+          <h1>Reflections</h1>
+
+          <div className="search_bar">
+            <input
+              type="search"
+              placeholder="Search reflections here..."
+            ></input>
+            <select name="" id="">
+              <option>Category</option>
+              <option>Web Design</option>
+              <option>App Design</option>
+              <option>App Development</option>
+            </select>
+            <select className="filter">
+              <option>Filter</option>
+            </select>
+          </div>
+
+          <div className="row-reflections">
+            <p>
+              You have <span>4</span> reflections in total
+            </p>
+            {/* Link component used for routing in React */}
+            <Link to="#">See all</Link>
+          </div>
+
+          {/* Repeated job card structure for each job */}
+          <div className="reflection_card">
+            <div className="reflection_details">
+              <div className="img">
+                <i className="fab fa-google-drive"></i>
               </div>
-              <div className="box">
-                <h3>CSS</h3>
-                <p>50% - progress</p>
-                <button>Continue</button>
-                <i className="fab fa-css3-alt css"></i>
-              </div>
-              <div className="box">
-                <h3>JavaScript</h3>
-                <p>30% - progress</p>
-                <button>Continue</button>
-                <i className="fab fa-js-square js"></i>
+              <div className="text">
+                <h2>UX Designer</h2>
+                <span>Google Drive - Junior Post</span>
               </div>
             </div>
+            <div className="reflection_salary">
+              <h4>$6.7 - $12.5k /yr</h4>
+              <span>1 days ago</span>
+            </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 }
