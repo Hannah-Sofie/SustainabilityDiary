d302cf606da642ad008d28842ea791886492694c
hannah-sofie <hannah-sofie@live.no>
2024-03-31 17:40:10 +0200

    Added loading indicator

---
+++ client/src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingIndicator.css";
+
+function LoadingIndicator() {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
