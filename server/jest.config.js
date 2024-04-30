611a6a46f38e740ac334a25c8ee2b06872d923fd
hannah-sofie <hannah-sofie@live.no>
2024-04-30 17:56:51 +0200

    testing

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   testEnvironment: "node",
-  testMatch: [
-    "**/test/**/*.js", // This pattern matches any JavaScript file inside any subdirectory under the test folder
-  ],
+  testMatch: [],
   coveragePathIgnorePatterns: ["/node_modules/"],
 };
