90363784c8eda64931323f718bd67a2f7119dfe3
Jonhels <jonhels8888@gmail.com>
2024-04-30 13:48:51 +0200

    test action

---
+++ client/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  testMatch: ["**/src/tests/jonhelge/**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js", // Mock static file imports
+  },
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest", // Add this line to handle .js and .jsx files
+  },
+};
