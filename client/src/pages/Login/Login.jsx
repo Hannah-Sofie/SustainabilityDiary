2acd745289403664083b829399516c2a353f351e
hannah-sofie <hannah-sofie@live.no>
2024-05-19 22:44:45 +0200

    organized files

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,110 @@
-import React from "react";
-import LoginForm from "../../components/LoginForm/LoginForm";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import LoginImage from "../../assets/img/login-img.png";
+import CustomButton from "../../components/CustomButton/CustomButton";
 
+// Login component for user authentication
 function Login() {
+  // State to store user credentials
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+  // Auth context to check if user is authenticated and perform login
+  const { isAuthenticated, login } = useAuth();
+
+  // Hook to navigate programmatically
+  const navigate = useNavigate();
+
+  // Redirect to dashboard if already authenticated
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
+
+  // Update state when form inputs change
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  // Handle form submission and perform login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await login(credentials);
+    if (success) {
+      navigate("/dashboard");
+    } else {
+      console.log("Login failed, not navigating");
+    }
+  };
+
   return (
     <div>
-      <LoginForm />
+      <div className="main-login">
+        {/* Button to go back to the home page */}
+        <div className="home-button">
+          <CustomButton
+            name="⬅ Back to home"
+            to="/"
+            backgroundColor="var(--darker-purple)"
+            color="var(--pure-white)"
+            hoverBackgroundColor="transparent"
+            hoverColor="var(--darker-purple)"
+            hoverBorderColor="var(--darker-purple)"
+          />
+        </div>
+
+        <div className="container-login">
+          <div className="login">
+            <h1>Login</h1>
+            <hr />
+            <p>Explore the World!</p>
+
+            {/* Login form */}
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={credentials.email}
+                  onChange={handleChange}
+                  placeholder="username@stud.ntnu.no"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={credentials.password}
+                  onChange={handleChange}
+                  placeholder="Enter your password"
+                  required
+                />
+              </div>
+              <p className="forgot-password">
+                <Link to="/forgot-password">Forgot password?</Link>
+              </p>
+              <button type="submit" id="login-button">
+                Login
+              </button>
+              <p className="no-account">
+                Don't have an account yet? <br />
+                <Link to="/register">Register here</Link>
+              </p>
+            </form>
+          </div>
+          <div className="login-pic">
+            <img src={LoginImage} alt="Login" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,175 @@
-import React from "react";
-import EditProfile from "../../components/EditProfile/EditProfile";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth } from "../../context/AuthContext";
+import { toast } from "react-toastify";
+import DefaultImage from "../../assets/img/default-profilepic.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import "./Profile.css";
 
 function Profile() {
+  const { userData, setUserData } = useAuth();
+  const [editMode, setEditMode] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [photo, setPhoto] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  // Initialize user info when the component mounts
+  useEffect(() => {
+    setUserInfo({ name: userData?.name, email: userData?.email });
+    setPreviewImage(null);
+  }, [userData]);
+
+  // Handle changes to the photo input
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0];
+    setPhoto(file);
+
+    if (file) {
+      setPreviewImage(URL.createObjectURL(file));
+    } else {
+      setPreviewImage(null);
+    }
+  };
+
+  // Handle changes to text inputs
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  // Handle form submission for profile update
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", userInfo.name);
+    formData.append("password", userInfo.password);
+
+    if (photo) {
+      formData.append("photo", photo);
+    }
+
+    try {
+      const response = await axios.put(
+        `${process.env.REACT_APP_API_URL}/api/users/update`,
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.data.status === "success") {
+        toast.success("Profile updated successfully!");
+        setUserData(response.data.user);
+        setEditMode(false);
+      } else {
+        throw new Error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(
+        "Failed to update profile: " +
+          (error.response?.data?.message || error.message)
+      );
+    }
+  };
+
   return (
     <div>
-      <EditProfile />
+      <div className="profile-container">
+        {editMode ? (
+          <form onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={userInfo.name}
+              onChange={handleChange}
+            />
+
+            <label>Email:</label>
+            <input type="email" name="email" value={userInfo.email} disabled />
+
+            <label>New Password (leave blank to keep the same):</label>
+            <input
+              type="password"
+              name="password"
+              value={userInfo.password}
+              onChange={handleChange}
+            />
+
+            <div className="file-upload">
+              <label
+                htmlFor="profile-photo-upload"
+                className="file-upload-label"
+              >
+                <FontAwesomeIcon icon={faUpload} /> Upload new profile photo
+              </label>
+              <input
+                id="profile-photo-upload"
+                type="file"
+                name="photo"
+                onChange={handlePhotoChange}
+                accept="image/*"
+                style={{ display: "none" }}
+              />
+
+              {previewImage && (
+                <img
+                  src={previewImage}
+                  alt="Profile Preview"
+                  className="profile-photo-preview"
+                />
+              )}
+            </div>
+
+            <button type="submit" className="save-changes">
+              Save changes
+            </button>
+            <button onClick={() => setEditMode(false)} className="cancel">
+              Cancel
+            </button>
+          </form>
+        ) : (
+          <div className="profile-view">
+            <p>
+              {userData?.photo ? (
+                <img
+                  src={`${process.env.REACT_APP_API_URL}/uploads/profilepics/${userData.photo}`}
+                  alt="Profile"
+                  className="profile-photo"
+                />
+              ) : (
+                <img
+                  src={DefaultImage}
+                  alt="Default profile"
+                  className="profile-photo"
+                />
+              )}
+            </p>
+
+            <p>
+              <span>Name:</span> {userInfo.name}
+            </p>
+            <p>
+              <span>Email:</span> {userInfo.email}
+            </p>
+            <p>
+              <span>Role:</span> {userData?.role}
+            </p>
+
+            <button onClick={() => setEditMode(true)} className="edit-profile">
+              Edit profile
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
