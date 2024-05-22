d40d4d6948351065e6f9bd6173fcf634975b69d8
hannah-sofie <hannah-sofie@live.no>
2024-05-22 12:30:25 +0200

    port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const connectDB = require("./dbconnect");
 
 const app = express();
 
-const PORT = 8093;
+const PORT = 8093; // process.env.PORT || 8003
 
 if (!process.env.MONGO_URI) {
   console.error("FATAL ERROR: MONGO_URI is not defined.");
